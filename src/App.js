796afb3fd34319cfd19fc728c4af1fa8751b44c6
IsaiahMitchell <zaymgmt707@gmail.com>
2025-05-05 13:07:30 -0700

    updates src

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm (Isaiah Mitchell)</h1>
+      <img
+        src="https://media.licdn.com/dms/image/v2/D4D03AQH7fIYxbfI4AQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1723759926492?e=1752105600&v=beta&t=wTJ653hBuc4FyVaa2vA4w0rT7n9njyDZ_KldtZtWrPA"
+        alt="image of Isaiah Mitchell"
+      />
+      <h2>About Me</h2>
+      <p>Software engineer</p>
+      <a href="https://github.com/isaiah2k">GitHub</a>
+      <a href="https://www.linkedin.com/in/isaiah-mitchel707/">LinkedIn</a>
+    </div>
+  );
 }
 
-export default App;
+export default App
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,48 @@
-import { render, screen } from "@testing-library/react";
-import '@testing-library/jest-dom';
+import { render, screen } from "@testing-library/react"
+import '@testing-library/jest-dom'
+import App from "../App"
 
-import App from "../App";
+test("displays a top-level heading with the text `Hi, I'm _______`", () => {
+  render(<App />)
+  const topLevelHeading = screen.getByRole("heading", {
+    name: /hi, i'm/i,
+    level: 1,
+  })
+  expect(topLevelHeading).toBeInTheDocument()
+})
 
-// Your tests here
+test("displays an image of myself with alt text", () => {
+  render(<App />)
+  const image = screen.getByAltText(/image of/i)
+  expect(image).toBeInTheDocument()
+  expect(image).toHaveAttribute("src")
+})
+
+test("displays a second-level heading with the text 'About Me'", () => {
+  render(<App />)
+  const aboutHeading = screen.getByRole("heading", {
+    name: /about me/i,
+    level: 2,
+  })
+  expect(aboutHeading).toBeInTheDocument()
+})
+
+test("displays a paragraph with a biography", () => {
+  render(<App />)
+  const paragraph = screen.getByText(/.+/, { selector: "p" })
+  expect(paragraph).toBeInTheDocument()
+})
+
+test("displays a link to my GitHub page", () => {
+  render(<App />)
+  const githubLink = screen.getByRole("link", { name: /github/i })
+  expect(githubLink).toBeInTheDocument()
+  expect(githubLink).toHaveAttribute("href")
+})
+
+test("displays a link to my LinkedIn page", () => {
+  render(<App />)
+  const linkedInLink = screen.getByRole("link", { name: /linkedin/i })
+  expect(linkedInLink).toBeInTheDocument()
+  expect(linkedInLink).toHaveAttribute("href")
+})
